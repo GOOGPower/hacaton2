3e7ef58f819a50c62efb0cdd20192a4dc609df9a
TempUser <agzamkin@mail.ru>
2025-10-16 21:08:03 +0300

    "scheme". "fe" into one page + data placeholders

--- src/ui/toolbar.ts
+++ src/ui/toolbar.ts
@@ -8,12 +8,13 @@ const fullscreenText = ["Полный экран", "Обычный режим"];
 export default function initToolbar() {
 	let eToolbar = document.getElementById('toolbar');
 	let eToggleFullscreen = document.getElementById('toggle-fullscreen');
+	let eContainer = document.getElementById('container');
 
 	if(eToggleFullscreen) {
 		eToggleFullscreen.textContent = fullscreenText[0];
 		eToggleFullscreen.onclick = () => {
 			fullscreen = !fullscreen;
-			if(fullscreen) document.body.requestFullscreen();
+			if(fullscreen && eContainer) eContainer.requestFullscreen();
 			else document.exitFullscreen();
 			eToggleFullscreen.textContent = fullscreenText[fullscreen?1:0];
 		};
